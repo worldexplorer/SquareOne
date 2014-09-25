0387eb1e6d374505cdaeb51c6c40e4a7d92f410f
worldexplorer <hamster>
2014-09-24 22:38:15 -0400

    +DataSeriesTimeBased uses SortedList<DateTime, double> only for AppendChecks and ContainsDate()  //following BarsUnscaledList approach
    +DataSeries.this[this.Count] moved to DataSeries.this[this.Count-1] and accessible by DataSeries.StreamingValue{get,set}
    +IndicatorMovingAverageSimple TEST_COMPATIBILITY passed

--- Sq1.Core/DataTypes/DataSeriesBasic.cs
+++ Sq1.Core/DataTypes/DataSeriesBasic.cs
@@ -1,42 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Sq1.Core.DataTypes {
 	public class DataSeriesBasic {
 		public string Description;
 		List<double> doubleValues;
 		public virtual IList<double> Values { get { return this.doubleValues; } }
-		public virtual double StreamingValue { get; set; }
-		public virtual int Count { get { return this.doubleValues.Count; } }
-		public virtual double LastStaticValue {
-			get {
-				if (this.Count == 0) return Double.NaN; 
-				return this[this.Count - 1];
-			}
-		}
-		protected int firstValidValueIndex;
-		public int FirstValidValueIndex {
-			get { return this.firstValidValueIndex; }
+		public virtual int StreamingIndex { get { return this.doubleValues.Count - 1; } }
+		public virtual double StreamingValue {
+			get { return (this.doubleValues.Count == 0) ? double.NaN : this[this.doubleValues.Count - 1]; }
 			set {
-				if (value < 0) {
-					this.firstValidValueIndex = 0;
-				} else {
-					this.firstValidValueIndex = value;
+				if (this.doubleValues.Count == 0) {
+					string msg = "USE_APPEND_BECAUSE_DOUBLE_VALUES_ARE_EMPTY_NOW; STREAMING_VALUE_IS_SIMPLY_LAST_ELEMENT";
+					#if DEBUG
+					Debugger.Break();
+					#endif
+					throw new Exception(msg);
 				}
-				if (this.firstValidValueIndex >= this.Count) {
-					this.firstValidValueIndex = this.Count - 1;
-				}
+				this[this.doubleValues.Count - 1] = value;
 			}
 		}
-		public double TotalSum { get { return this.TotalSumTillIndex(this.Count - 1); } }
+		public virtual int Count { get { return this.doubleValues.Count; } }
+//		public virtual double LastStaticValue {
+//			get {
+//				if (this.Count == 0) return Double.NaN; 
+//				return this[this.Count - 1];
+//			}
+//		}
+//		protected int firstValidValueIndex;
+//		public int FirstValidValueIndex {
+//			get { return this.firstValidValueIndex; }
+//			set {
+//				if (value < 0) {
+//					this.firstValidValueIndex = 0;
+//				} else {
+//					this.firstValidValueIndex = value;
+//				}
+//				if (this.firstValidValueIndex >= this.Count) {
+//					this.firstValidValueIndex = this.Count - 1;
+//				}
+//			}
+//		}
+//		public double TotalSum { get { return this.TotalSumTillIndex(this.Count - 1); } }
 		public virtual int Capacity {
 			get { return this.doubleValues.Capacity; }
 			set { this.doubleValues.Capacity = value; }
 		}
 		public DataSeriesBasic() {
 			this.doubleValues = new List<double>();
-			this.firstValidValueIndex = 0;
-			this.StreamingValue = double.NaN;
+//			this.firstValidValueIndex = 0;
+			#if DEBUG
+			if (double.IsNaN(this.StreamingValue) == false) {
+				Debugger.Break();
+			}
+			#endif
 		}
 		public DataSeriesBasic(string description) : this() {
 			this.Description = description;
@@ -49,8 +67,10 @@ namespace Sq1.Core.DataTypes {
 					throw new ArgumentOutOfRangeException(msg);
 				}
 				if (barIndex == this.doubleValues.Count) {
-					// get rid of this call, it's inaccurate
-					return this.doubleValues[barIndex - 1];
+					string msg = "DEPRECATED_NOTATION this[this.Count]_get; STREAMING_VALUE_IS_NOW_AT_this[this.Count-1]";
+					Debugger.Break();
+					throw new Exception(msg);
+					//return this.doubleValues[barIndex - 1];
 				}
 				return this.doubleValues[barIndex];
 			}
@@ -60,42 +80,53 @@ namespace Sq1.Core.DataTypes {
 					throw new ArgumentOutOfRangeException(msg);
 				}
 				if (barIndex == this.doubleValues.Count) {
-					this.StreamingValue = value;
-					return;
+					string msg = "DEPRECATED_NOTATION this[this.Count]_set; STREAMING_VALUE_IS_NOW_AT_this[this.Count-1]";
+					Debugger.Break();
+					throw new Exception(msg);
+					//this.StreamingValue = value;
+					//return;
 				}
 				this.doubleValues[barIndex] = value;
 			}
 		}
-		public virtual void Add(double value) {
-			this.doubleValues.Add(value);
-		}
-		public virtual void Insert(int index, double value) {
-			this.doubleValues.Insert(index, value);
-		}
 		public virtual void Clear() {
 			this.doubleValues.Clear();
 		}
-		public virtual void RemoveAt(int index) {
-			this.doubleValues.RemoveAt(index);
+		public virtual void Append(double value) {
+			this.doubleValues.Add(value);
 		}
-		public virtual double TotalSumTillIndex(int indexTill) {
-			if (this.Count == 0) {
-				return 0.0;
+//		public virtual void Insert(int index, double value) {
+//			this.doubleValues.Insert(index, value);
+//		}
+//		public virtual void RemoveAt(int index) {
+//			this.doubleValues.RemoveAt(index);
+//		}
+//		public virtual double TotalSumTillIndex(int indexTill) {
+//			if (this.Count == 0) {
+//				return 0.0;
+//			}
+//			double ret = 0;
+//			for (int i = this.FirstValidValueIndex; i < this.Count; i++) {
+//				ret += this.doubleValues[i];
+//			}
+//			return ret;
+//		}
+		// bool handlingVolume = indicator.Series.TypeSafeIsProxyFor(this.Executor.Bars, DataSeriesProxyableFromBars.Volume)
+//		public virtual bool TypeSafeIsProxyFor(Bars bars, DataSeriesProxyableFromBars barField) {
+//			if (this is DataSeriesProxyBars == false) return false;
+//			DataSeriesProxyBars proxy = this as DataSeriesProxyBars;
+//			if (proxy.IsProxyFor(bars, barField)) return true;
+//			return false;
+//		}
+		public override string ToString() {
+			string ret = this.Count + "doubleValues";
+			if (this.Count > 0) {
+				ret += " last=[" + this.StreamingValue + "] @[" + this.StreamingIndex + "]";
 			}
-			double ret = 0;
-			for (int i = this.FirstValidValueIndex; i < this.Count; i++) {
-				ret += this.doubleValues[i];
-			}
+			if (string.IsNullOrEmpty(this.Description) == false) ret += "/" + this.Description;
 			return ret;
 		}
-		// bool handlingVolume = indicator.Series.TypeSafeIsProxyFor(this.Executor.Bars, DataSeriesProxyableFromBars.Volume)
-		public virtual bool TypeSafeIsProxyFor(Bars bars, DataSeriesProxyableFromBars barField) {
-			if (this is DataSeriesProxyBars == false) return false;
-			DataSeriesProxyBars proxy = this as DataSeriesProxyBars;
-			if (proxy.IsProxyFor(bars, barField)) return true;
-			return false;
-		}
-		
+	
 		public bool TurnsDownAtBarIndex(int barIndex) {
 			if (barIndex <= 0) return false;
 			if (this.Count < 2) return false;
--- Sq1.Core/DataTypes/DataSeriesTimeBased.cs
+++ Sq1.Core/DataTypes/DataSeriesTimeBased.cs
@@ -1,112 +1,146 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Sq1.Core.DataTypes {
 	public class DataSeriesTimeBased : DataSeriesBasic {
 		SortedList<DateTime, double> doublesByDate;
-		public virtual IList<DateTime> DateTimes { get { return this.doublesByDate.Keys; } }
-		public override IList<double> Values { get { return this.doublesByDate.Values; } }
-		public override int Count { get { return this.doublesByDate.Count; } }
-		public override int Capacity {
-			get { return this.doublesByDate.Capacity; }
-			set { this.doublesByDate.Capacity = value; }
-		}
+		public DateTime LastDateAppended;
+
+//			{ get {
+//				if (this.DateTimes.Count <= 0) return DateTime.MinValue;
+//				return this.DateTimes[this.DateTimes.Count - 1];
+//			} }
+//		public override double TotalSumTillIndex(int indexTill) {
+//			if (this.Count == 0) return 0.0;
+//			double ret = 0;
+//			for (int i = 0; i < this.Count; i++) ret += this.doublesByDate.Values[i];
+//			return ret;
+//		}
+
+//		public virtual IList<DateTime> DateTimes { get { return this.doublesByDate.Keys; } }
+//		public override IList<double> Values { get { return this.doublesByDate.Values; } }
+//		public override int Count { get { return this.doublesByDate.Count; } }
+//		public override int Capacity {
+//			get { return this.doublesByDate.Capacity; }
+//			set { this.doublesByDate.Capacity = value; }
+//		}
 		public BarScaleInterval ScaleInterval;
-		public bool IsIntraday { get { return this.ScaleInterval.IsIntraday; } }
+//		public bool IsIntraday { get { return this.ScaleInterval.IsIntraday; } }
 
 		public DataSeriesTimeBased(BarScaleInterval scaleInterval) {	// : base()
-			this.doublesByDate = new SortedList<DateTime, double>();
-			this.firstValidValueIndex = 0;
-			this.StreamingValue = double.NaN;
-			this.ScaleInterval = scaleInterval;
+			doublesByDate = new SortedList<DateTime, double>();
+//			firstValidValueIndex = 0;
+			ScaleInterval = scaleInterval;
+			LastDateAppended = DateTime.MinValue;
 		}
 		public DataSeriesTimeBased(BarScaleInterval scaleInterval, string description) : this(scaleInterval) {
 			this.Description = description;
 		}
-		public double this[DateTime dateTime] { get {
-				if (this.doublesByDate.ContainsKey(dateTime) == false) {
-					DateTime existingKeyFromBelow = FindExistingKeyFromBelow(dateTime);
-					DateTime existingKeyFromAbove = FindExistingKeyFromAbove(dateTime);
-					string msg = "KEY_NOT_FOUND[" + dateTime + "]_CLOSEST_[" + existingKeyFromBelow + "]_[" + existingKeyFromAbove + "]: " + this;
-					throw new ArgumentException(msg);
-				}
-				return this.doublesByDate[dateTime];
-			} }
-		public DateTime FindExistingKeyFromBelow(DateTime dateNonExistingRequested) {
-			DateTime ret = DateTime.MinValue;
-			foreach (DateTime each in this.doublesByDate.Keys) {
-				if (dateNonExistingRequested > each) continue;
-				if (ret <= each) ret = each;
-			}
-			return ret;
-		}
-		public DateTime FindExistingKeyFromAbove(DateTime dateNonExistingRequested) {
-			DateTime ret = DateTime.MaxValue;
-			foreach (DateTime each in this.doublesByDate.Keys) {
-				if (dateNonExistingRequested < each) continue;
-				if (ret >= each) ret = each;
-			}
-			return ret;
-		}
-		public bool ContainsKey(DateTime dateTime) {
+//		public double this[DateTime dateTime] { get {
+//				if (this.doublesByDate.ContainsKey(dateTime) == false) {
+//					DateTime existingKeyFromBelow = FindExistingKeyFromBelow(dateTime);
+//					DateTime existingKeyFromAbove = FindExistingKeyFromAbove(dateTime);
+//					string msg = "KEY_NOT_FOUND[" + dateTime + "]_CLOSEST_[" + existingKeyFromBelow + "]_[" + existingKeyFromAbove + "]: " + this;
+//					throw new ArgumentException(msg);
+//				}
+//				return this.doublesByDate[dateTime];
+//			} }
+//		public DateTime FindExistingKeyFromBelow(DateTime dateNonExistingRequested) {
+//			DateTime ret = DateTime.MinValue;
+//			foreach (DateTime each in this.doublesByDate.Keys) {
+//				if (dateNonExistingRequested > each) continue;
+//				if (ret <= each) ret = each;
+//			}
+//			return ret;
+//		}
+//		public DateTime FindExistingKeyFromAbove(DateTime dateNonExistingRequested) {
+//			DateTime ret = DateTime.MaxValue;
+//			foreach (DateTime each in this.doublesByDate.Keys) {
+//				if (dateNonExistingRequested < each) continue;
+//				if (ret >= each) ret = each;
+//			}
+//			return ret;
+//		}
+		public bool ContainsDate(DateTime dateTime) {
 			return this.doublesByDate.ContainsKey(dateTime);
 		}
-		[Obsolete("USE_DATETIME_KEYS_FOR_FASTER_ACCESS SortedList is a hash-based data structure")]
-		public override double this[int barIndex] {
-			get {
-				if (barIndex < 0 || barIndex > this.Count) {
-					string msg = "[" + barIndex + "] is out of bounds: " + this;
-					throw new ArgumentOutOfRangeException(msg);
-				}
-				if (barIndex == this.doublesByDate.Count) {
-					return base.StreamingValue;
-				}
-				return this.doublesByDate.Values[barIndex];
-			}
-			set {
-				if (barIndex < 0 || barIndex > this.Count) {
-					string msg = "[" + barIndex + "] is out of bounds: " + this;
-					throw new ArgumentOutOfRangeException(msg);
-				}
-				if (barIndex == this.doublesByDate.Count) {
-					base.StreamingValue = value;
-					return;
-				}
-				DateTime dateKey = this.doublesByDate.Keys[barIndex];
-				this.doublesByDate[dateKey] = value;
-			}
-		}
+//		[Obsolete("USE_BASE_THIS[INT]_DONT_USE_DATETIME_KEYS_ITS_SLOW!!! after profiling you'll see lot of SortedList.BinarySearch()")]
+//		public override double this[int barIndex] {
+//			get {
+//				if (barIndex < 0 || barIndex > this.Count) {
+//					string msg = "[" + barIndex + "] is out of bounds: " + this;
+//					throw new ArgumentOutOfRangeException(msg);
+//				}
+//				if (barIndex == this.doublesByDate.Count) {
+//					return base.StreamingValue;
+//				}
+//				return this.doublesByDate.Values[barIndex];
+//			}
+//			set {
+//				if (barIndex < 0 || barIndex > this.Count) {
+//					string msg = "[" + barIndex + "] is out of bounds: " + this;
+//					throw new ArgumentOutOfRangeException(msg);
+//				}
+//				if (barIndex == this.doublesByDate.Count) {
+//					base.StreamingValue = value;
+//					return;
+//				}
+//				DateTime dateKey = this.doublesByDate.Keys[barIndex];
+//				this.doublesByDate[dateKey] = value;
+//			}
+//		}
 		public virtual void Append(DateTime dateTimeAdding, double value) {
 			try {
 				this.checkThrowDateAlreadyAdded(dateTimeAdding);
-				//this.CheckThrowDatePriorToLast(dateTimeAdding);
+				this.checkThrowDatePriorToLast(dateTimeAdding);
+				base.Append(value);
 				this.doublesByDate.Add(dateTimeAdding, value);
 			} catch (Exception e) {
-				int a = 1;
+				#if DEBUG
+				Debugger.Break();
+				#endif
 				throw e;
 			}
 		}
-		public override void Insert(int index, double value) {
-			throw new Exception("Can't insert by index, use Add(DateTime, double) instead");
-		}
 		public override void Clear() {
+			base.Clear();
 			this.doublesByDate.Clear();
+			this.LastDateAppended = DateTime.MinValue;
 		}
-		public override void RemoveAt(int index) {
-			this.doublesByDate.RemoveAt(index);
-		}
-		private void checkThrowDateAlreadyAdded(DateTime adding) {
-			if (doublesByDate.ContainsKey(adding) == false) return;
-			string msg = "#2 Can not append time[" + adding + "] which already was added as [" + this.doublesByDate[adding] + "]";
+//		public override void Insert(int index, double value) {
+//			throw new Exception("Can't insert by index, use Add(DateTime, double) instead");
+//		}
+//		public override void RemoveAt(int index) {
+//			this.doublesByDate.RemoveAt(index);
+//		}
+		private void checkThrowDateAlreadyAdded(DateTime appending) {
+			if (this.doublesByDate.ContainsKey(appending) == false) return;
+			string msg = "#2 Can not append time[" + appending + "] which already was added as [" + this.doublesByDate[appending] + "]";
+			#if DEBUG
+			Debugger.Break();
+			#endif
 			throw new Exception(msg);
 		}
-		protected void CheckThrowDatePriorToLast(DateTime appending) {
-			if (this.DateTimes.Count == 0) return;
-			DateTime lastDateTime = this.LastStaticDate;
-			if (lastDateTime.Ticks < appending.Ticks) return;
+		private  void checkThrowDatePriorToLast(DateTime appending) {
+			if (this.LastDateAppended == DateTime.MinValue) return;
+			if (this.LastDateAppended < appending) return;
 
-			string msg = "#3 Can not append time[" + appending + "] should be > this.Date["
-				+ (this.DateTimes.Count - 1) + "]=[" + lastDateTime + "]";
+			string msg = "#3 Can not append time[" + appending + "] should be > this.LastStaticDate["
+				+ this.LastDateAppended + "]=[" + (this.doublesByDate.Count - 1) + "]=[" + this.LastDateAppended + "]";
+			#if DEBUG
+			Debugger.Break();
+			#endif
+			throw new Exception(msg);
+		}
+		private void checkThrowLastStaticDateEqualsToAppended(DateTime appending) {
+			if (this.doublesByDate.Count == 0) return; 
+			if (this.doublesByDate.Keys[this.doublesByDate.Count - 1] == this.LastDateAppended) return;
+			string msg = "#4 Can not append time[" + appending + "] this.LastStaticDate[" + this.LastDateAppended
+				+ "] should be = LastAppended[" + this.doublesByDate.Keys[this.doublesByDate.Count - 1] + "]";
+			#if DEBUG
+			Debugger.Break();
+			#endif
 			throw new Exception(msg);
 		}
 		public void SumupOrAppend(DateTime dateTimeAdding, double value) {
@@ -119,25 +153,9 @@ namespace Sq1.Core.DataTypes {
 			valueExisting += value;
 			this[indexFound] = valueExisting;
 		}
-		public DateTime LastStaticDate {
-			get {
-				if (this.DateTimes.Count <= 0) return DateTime.MinValue;
-				return this.DateTimes[this.DateTimes.Count - 1];
-			}
-		}
-		public override double TotalSumTillIndex(int indexTill) {
-			if (this.Count == 0) return 0.0;
-			double ret = 0;
-			for (int i = 0; i < this.Count; i++) ret += this.doublesByDate.Values[i];
-			return ret;
-		}
 		public override string ToString() {
-			string ret = "[" + this.ScaleInterval + "]" + this.Count + "doublesByDate";
-			if (this.Count > 0) {
-				int lastBar = this.Count - 1;
-				ret += " last=[" + this[lastBar] + "] @[" + this.DateTimes[lastBar] + "]";
-			}
-			if (string.IsNullOrEmpty(base.Description) == false) ret += "/" + base.Description;
+			string ret = "[" + this.ScaleInterval + "]" + this.Count + "doublesByDate ";
+			ret += base.ToString();
 			return ret;
 		}
 
