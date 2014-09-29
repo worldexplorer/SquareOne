d3b42eef924e1c2d8275523feba4012ca7686151
worldexplorer <hamster>
2014-09-29 19:42:45 -0400

    Sq1.Gui.Widgets\ -> Sq1.Widgets\

---
+++ Sq1.Core/Charting/MultiSplitterProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json.Serialization;
+
+namespace Sq1.Charting.MultiSplit {
+	public class MultiSplitterProperties {
+		//[JsonProperty]
+		public int ManualOrder;
+		//[JsonProperty]
+		public int Distance;
+		
+		public MultiSplitterProperties(int manualOrder, int distance) {
+			ManualOrder = manualOrder;
+			Distance = distance;
+		}
+	}
+}
