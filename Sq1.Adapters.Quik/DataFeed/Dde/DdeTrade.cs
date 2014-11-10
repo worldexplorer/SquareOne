e4f74a1d8072d6f2a41c06bd4452828dd7f259c2
worldexplorer <hamster>
2014-11-09 19:46:56 -0500

    +fresh autogen_Minute-1.bar, ready to branch off dev to v1.0-quik

--- Sq1.Adapters.Quik/DataFeed/Dde/DdeTrade.cs
+++ Sq1.Adapters.Quik/DataFeed/Dde/DdeTrade.cs
@@ -1,4 +1,4 @@
-﻿// QScalp source code was downloaded on Apr 2012 for free from http://www.qscalp.ru/download
+﻿// QScalp source code was downloaded on O2-Jun-2012 for free from http://www.qscalp.ru/download/qscalp_src.zip
 // SquareOne uses QScalp's modified classes and keeps original author Name and URL
 // Nikolay Moroshkin can tell me to remove his code completely => I'll rewrite the pieces borrowed //Pavel Chuchkalov 
 using System;
